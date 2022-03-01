4251c17499f052049404a9df700a60cc5b5ac635
alara505 <alex.lara@revature.net>
2022-03-01 15:00:55 -0600

    Update CustomerDAOImp.java

--- Project_0/main/src/com/data_access_layer/CustomerDAO/CustomerDAOImp.java
+++ Project_0/main/src/com/data_access_layer/CustomerDAO/CustomerDAOImp.java
@@ -2,9 +2,20 @@ package com.data_access_layer.CustomerDAO;
 
 import com.entities.customer;
 
+import java.util.List;
+
 
 public interface CustomerDAOImp {
 
 
     customer getCustomerById(int customerId);
+
+    customer createCustomer(customer customer);
+
+    List<customer> getAllCustomers();
+
+    customer updateCustomerById(customer customer);
+
+    Boolean deleteCustomer(int customerId);
+    
 }
